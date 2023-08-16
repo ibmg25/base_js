6dfdcd0e246f7ea9c73d7580e7445ab94825fc1c
Ignacio <ignacio.martinez@ucb.edu.bo>
2023-08-16 17:31:17 -0400

    tercer commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-alert("Hola desde Javascript");
+alert("Presiona OK si estas list@ para empezar con javascript");
 
 /*
-git remote add origin "https://github.com/ibmg25"
+git remote add origin "https://github.com/ibmg25/base_js.git"  
 git branch -M main
 git push -u origin main
 */
